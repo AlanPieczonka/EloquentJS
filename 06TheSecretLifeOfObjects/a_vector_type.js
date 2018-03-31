c3f1ae3f88523db26ab37eae671968894e5db070
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-31 12:54:46 +0200

    Finish 06TheSecretLifeOfObjects/a_vector_type exercise

---
+++ 06TheSecretLifeOfObjects/a_vector_type.js
@@ -0,0 +1,29 @@
+class Vec {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+  
+  plus(vector) {
+    return new Vec((this.x + vector.x), (this.y + vector.y));
+  }
+  
+  minus(vector) {
+    return new Vec((this.x - vector.x), (this.y - vector.y));
+  }
+  
+  get length() {
+    const powerOfX = this.x ** 2,
+          powerOfY = this.y ** 2;
+          
+          
+    return Math.sqrt((powerOfX + powerOfY));
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
